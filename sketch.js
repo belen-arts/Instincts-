b8ef5aa594ed54aafb474e59c2e816a67a6f9d0f
Belén Fernández <b.fernandez0320221@arts.ac.uk>
2024-12-27 18:35:43 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,2 +1,9 @@
 let serial; // Variable to hold serial connection
 let shape = ""; // Current shape to draw
+
+function setup() {
+  createCanvas(800, 800);
+  serial = new p5.SerialPort(); // Create a new serial port instance
+  serial.open('/dev/ttyUSB0'); // Replace with Arduino port.
+  serial.on('data', serialEvent); // Callback for when new data arrives
+}
