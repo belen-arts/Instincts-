39296c8453d0de038d7767fd6bb3020eb030a4ad
Belén Fernández <b.fernandez0320221@arts.ac.uk>
2024-12-30 12:58:38 +0100

    attempting shapes readings

--- sketch.js
+++ sketch.js
@@ -1,55 +1,42 @@
 let port;
 let connectBtn;
-let shape = ""; // Current shape to draw
+let myVal = 0;
+let shape = "";
 
 function setup() {
   createCanvas(800, 800);
+
   port = createSerial();
-  port.open('Arduino', 9600);
-  let usedPorts = usedSerialPorts();
-if (usedPorts.length > 0) {
-  port.open(usedPorts[0], 9600);
-}
-connectBtn = createButton('Connect to Arduino');
-connectBtn.position(80, 200);
-connectBtn.mousePressed(connectBtnClick);
-}
+
+  connectBtn = createButton('Connect to Arduino');
+  connectBtn.position(80, 200);
+  connectBtn.mousePressed(connectBtnClick);
 
 
-// function draw() {
-//   background(220);
-//   // Draw shape based on resistor classification
-//   fill(100, 150, 255);
-//   stroke(0);
-//   strokeWeight(2);
+  function draw() {
+    background(220);
+    fill(50);
+    rect(50, 50, 50, 50);
 
-//   if (port.opened()) {
-//     // the port is indeed open
-//   }
-//   let str = port.last();
+    let shape = port.readUntil("\n").trim(); // Read each line and trim whitespace
+   
+    // Shapes.. 
+    if (shape === "circle") {
+      ellipse(width / 2, height / 2, 100, 100); // Circle
+    } else if (shape === "square") {
+      square(width / 2 - 50, height / 2 - 50, 100, 100); // Square
+    } else if (shape === "triangle") {
+      triangle(width / 2, height / 2 - 50, width / 2 - 50, height / 2 + 50, width / 2 + 50, height / 2 + 50); // Triangle
+    } else if (shape === "rectangle") {
+      rect(width / 2 - 50, height / 2 - 50, 200, 100); // Rectangle
+    }
 
-//   if (shape === "circle") {
-//     ellipse(width / 2, height / 2, 100, 100); // Circle
-//   } else if (shape === "square") {
-//     rect(width / 2 - 50, height / 2 - 50, 100, 100); // Square
-//   } else if (shape === "triangle") {
-//     triangle(width / 2, height / 2 - 50, width / 2 - 50, height / 2 + 50, width / 2 + 50, height / 2 + 50); // Triangle
-//   } else if (shape === "rectangle") {
-//     rect(width / 2 - 50, height - 50, 200, 200); // Rectangle
-//     }
-//     endShape(CLOSE);
-//   }
-
-//   function serialEvent() {
-//     if (data) {
-//       shape = data; // Update shape based on resistor classification
-//     }
-  
-
-function connectBtnClick() {
-  if (!port.opened()) {
-    port.open('Arduino', 9600);
-  } else {
-    port.close();
+  function connectBtnClick() {
+    if (!port.opened()) {
+      port.open('Arduino', 9600);
+    } else {
+      port.close();
+    }
   }
+}
 }
