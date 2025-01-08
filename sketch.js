f880f782415eba50051d860a8e4c57c97e4caf68
Belén Fernández <b.fernandez0320221@arts.ac.uk>
2025-01-08 22:28:38 +0100

    initial page setup
    when value is '0'

--- sketch.js
+++ sketch.js
@@ -4,9 +4,9 @@ let shape  = 0; // Declare shape as a int not a string!
 
 function setup() {
   createCanvas(windowHeight, windowWidth);
- port = createSerial();
+  port = createSerial();
   connectBtn = createButton('Connect to Arduino');
-  connectBtn.position(80, 200);
+  connectBtn.position(20, 20);
   connectBtn.mousePressed(connectBtnClick);
 }
 function draw() {
@@ -24,18 +24,25 @@ if (receivedData) {
     fill(0, 255, 0); // Green color
     ellipse(width / 2, height / 2, 150, 150); // Circle shape
   } else if (shape === 3) {
-    fill(0, 0, 255); // Blue color
+    fill('#07C'); // Blue color
     rect(width / 2 - 75, height / 2 - 75, 150, 150); // Square shape
   } else if (shape === 4) {
     fill(255, 255, 0); // Yellow color
     triangle(width / 2 - 75, height / 2 + 75, width / 2 + 75, height / 2 + 75, width / 2, height / 2 - 75); // Triangle shape
   } else {
-    textSize(32);
     fill(0);
-    text("No Shape", width / 2 - 80, height / 2); // Display no shape message
+    textSize(32);
+    text("INSERT TWO SHAPES.", 250, 400); 
+    fill(255, 0, 0);
+    rect(50, 550,200, 100);
+    fill (0,255,0);
+    ellipse(350, 600, 100, 100, 650, 550);
+    fill('#07C');
+    rect(450,525,150,150);
+    fill(255,255,0);
+    triangle(700, 600, 800, 600, 750, 500);
   }
 }
-
 function connectBtnClick() {
   if (!port.opened()) {
     console.log("Opening port...");
