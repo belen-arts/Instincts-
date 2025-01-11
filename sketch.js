08a9069eed2280943eab6a5e39f94b8a6da5117a
Belén Fernández <b.fernandez0320221@arts.ac.uk>
2025-01-11 20:06:10 +0100

    initial page
    'welcome' page to the simulation.

--- sketch.js
+++ sketch.js
@@ -19,23 +19,19 @@ function draw() {
 
 let receivedData = port.readUntil('\n');
 if (receivedData) {
-  shape = parseInt(receivedData.trim()); 
+  shape = parseInt(receivedData.trim()); // convert string to array 
   console.log("Received Shape Value: " + shape);  
 }
   processShapeData ();  
-  if (shape === 1) {
-    fill(255, 0, 0); 
-    rect(width / 2 - 100, height / 2 - 50, 200, 100); // Rectangle 
-  } else if (shape === 2) {
-    fill(0, 255, 0); // Green 
-    ellipse(width / 2, height / 2, 150, 150); // Circle 
-  } else if (shape === 3) {
-    fill('#07C'); // Blue 
-    rect(width / 2 - 75, height / 2 - 75, 150, 150); // Square 
-  // } else if (shape === 4) {
-  //   fill(255, 255, 0); // Yellow 
-  //   triangle(width / 2 - 75, height / 2 + 75, width / 2 + 75, height / 2 + 75, width / 2, height / 2 - 75); // Triangle 
-  } else { 
+
+if (selectedShapes.length < 2) {  
+  initialShapes();
+} else {
+  displayAnimals(); 
+}
+}
+
+function initialShapes() { 
     fill(0); 
     textSize(32);
     text("INSERT TWO SHAPES.", 250, 400); 
@@ -48,11 +44,10 @@ if (receivedData) {
     fill(255,255,0);
     triangle(700, 600, 800, 600, 750, 500);
   } 
-}
 
 function drawRectangle(x, y, size) {
   fill(255, 0, 0);
-  rect(x, y, size, size / 2); // rectangle to draw 
+  rect(x - size / 2, y - size / 4, size, size / 2); // rectangle to draw 
 } 
 function drawCircle(x, y, size) {
   fill(0, 255, 0);
@@ -60,11 +55,14 @@ function drawCircle(x, y, size) {
 }
 function drawSquare(x, y, size) {
   fill('#07C');
-  rect(x, y, size, 150, 150); // square to draw 
+  rect(x - size / 2, y - size / 2, size, size); // square to draw 
 }
 function drawTriangle(x, y, size) {
   fill(255, 255, 0);
-  triangle(x - 75, y + 75, x + 75, y + 75, x, y - 75); // triangle to draw 
+  triangle(x - size / 2, y + size / 2, // bottom left corner
+    x + size / 2, y + size / 2, // bottom right corner
+    x, y - size / 2 // top corner
+  ); // triangle to draw 
 }
 
 function processShapeData() {
@@ -77,14 +75,12 @@ function processShapeData() {
   }
    if (selectedShapes.length === 2) {
     combineShapes();
-    selectedShapes = []; // clear the selected shapes array after the shapes have been combined.
    }
   } else {
   console.log("No shapes selected.");
 }
 }
   function combineShapes() { // moved outside of draw to avoid being called multiple times. 
-    console.log("Combining shapes???");
     if (selectedShapes.length === 2) {
   let Head = drawSelectedShapes[selectedShapes[0] - 1]; // !! important to relate the shape to draw to the correct index in the array of selected shapes eg. 4 - 1 = 3 so the triangle will be drawn
   let Body = drawSelectedShapes [selectedShapes[1] - 1]; // this gives you the second shape.
@@ -101,7 +97,8 @@ function processShapeData() {
   // draw the head and body...
   Head(headX, headY, headSize);
   Body(bodyX, bodyY, bodySize);
-
+  
+  selectedShapes = []; // clear the array so it can be used again.
   console.log("Animal created! Head: " + selectedShapes[0] + " Body: " + selectedShapes[1]);
   }
   }
